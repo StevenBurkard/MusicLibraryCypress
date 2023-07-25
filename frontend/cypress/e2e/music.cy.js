fe03e54053be8fd1cbab58200862744d319dedec
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-07-25 10:12:58 -0500

    Added Cypress test for searchBar functionality

--- frontend/cypress/e2e/music.cy.js
+++ frontend/cypress/e2e/music.cy.js
@@ -1,13 +1,23 @@
 describe('Music Library app', () => {
-  it('Adds a new song', () => {
-    cy.visit('http://localhost:3000/')
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/')
+    });
+    
+    it('Adds a new song', () => {
+      cy.get('[data-cy=song-title]').type('Sabotage');
+      cy.get('[data-cy=song-artist]').type('Beastie Boys');
+      cy.get('[data-cy=song-album]').type('III Communication');
+      cy.get('[data-cy=song-genre]').type('Punk Rock');
+      cy.get('[data-cy=song-release-date]').type('1994-01-28');
+      cy.get('[data-cy=song-running-time]').type('343');
+      cy.get('[data-cy=add-song-button]').click();
+    });
 
-    cy.get('[data-cy=song-title]').type('Sabotage');
-    cy.get('[data-cy=song-artist]').type('Beastie Boys');
-    cy.get('[data-cy=song-album]').type('III Communication');
-    cy.get('[data-cy=song-genre]').type('Punk Rock');
-    cy.get('[data-cy=song-release-date]').type('1994-01-28');
-    cy.get('[data-cy=song-running-time]').type('343');
-    cy.get('[data-cy=add-song-button]').click();
-  })
-})
+    it('Filters song based on search term', () => {
+      const searchTerm = 'Sabotage';
+
+      cy.get('[data-cy=search-bar]').type(searchTerm).wait(1000);
+      cy.get('table').contains('td', searchTerm).should('be.visible');
+    });
+  
+});
